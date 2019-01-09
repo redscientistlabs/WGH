a2ef7560018857521de669073aef6e0108758e61
Phil Girard <phil@redscientist.com>
2019-01-08 20:55:53 -0500

    oops forgot progress bar

---
+++ WindowsGlitchHarvester/WGH_Progress.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WindowsGlitchHarvester
+{
+    partial class WGH_Progress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbProgress = new System.Windows.Forms.ProgressBar();
+            this.lbProgress = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pbProgress
+            // 
+            this.pbProgress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbProgress.Location = new System.Drawing.Point(51, 42);
+            this.pbProgress.Name = "pbProgress";
+            this.pbProgress.Size = new System.Drawing.Size(304, 16);
+            this.pbProgress.TabIndex = 0;
+            // 
+            // lbProgress
+            // 
+            this.lbProgress.AutoSize = true;
+            this.lbProgress.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold);
+            this.lbProgress.ForeColor = System.Drawing.Color.White;
+            this.lbProgress.Location = new System.Drawing.Point(48, 60);
+            this.lbProgress.Name = "lbProgress";
+            this.lbProgress.Size = new System.Drawing.Size(22, 21);
+            this.lbProgress.TabIndex = 1;
+            this.lbProgress.Text = "...";
+            // 
+            // WGH_Progress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(426, 207);
+            this.Controls.Add(this.lbProgress);
+            this.Controls.Add(this.pbProgress);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "WGH_Progress";
+            this.Text = "WGH_Progress";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ProgressBar pbProgress;
+        private System.Windows.Forms.Label lbProgress;
+    }
+}
---
+++ WindowsGlitchHarvester/WGH_Progress.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsGlitchHarvester
+{
+    public partial class WGH_Progress : Form
+    {
+        public BackgroundWorker bw = new BackgroundWorker();
+        string defaultLabel;
+
+        public WGH_Progress(string lbText, int maxprogress, Action<object, EventArgs> registrant)
+        {
+            InitializeComponent();
+
+            BackColor = WGH_Core.ghForm.BackColor;
+
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+
+            defaultLabel = lbText;
+            //lbProgress.Text = defaultLabel;
+
+            pbProgress.Minimum = 0;
+            pbProgress.Value = 0;
+            pbProgress.Maximum = maxprogress;
+
+            bw.DoWork += registrant.Invoke;
+            bw.ProgressChanged += Bw_ProgressChanged;
+            bw.RunWorkerCompleted += Bw_RunWorkerCompleted;
+
+            TopLevel = false;
+            Size = WGH_Core.ghForm.Size;
+
+            WGH_Core.ghForm.Controls.Add(this);
+            Anchor = (AnchorStyles)(AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Top | AnchorStyles.Bottom);
+            Show();
+            BringToFront();
+
+
+
+        }
+
+        public void Run()
+        {
+            bw.RunWorkerAsync();
+        }
+
+        private void Bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            bw.Dispose();
+            this.Hide();
+            WGH_Core.progressForm = null;
+        }
+
+        private void Bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage != 0)
+            {
+                if (pbProgress.Value + 1000 < pbProgress.Maximum)
+                    pbProgress.Value += 1000;
+                else
+                    pbProgress.Value = pbProgress.Maximum;
+            }
+
+            if (e.UserState != null && e.UserState is string)
+            {
+                if((e.UserState as string) == "DEFAULT")
+                    lbProgress.Text = defaultLabel;
+                else
+                    lbProgress.Text = (e.UserState as string);
+            }
+
+        }
+
+    }
+}
